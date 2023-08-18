ba59c754b40573874bf7d063a0cfef170aceb8b4
Pedro Donato <emagdonatdonatoordep@gmail.com>
2023-08-18 15:08:50 -0300

    corsss grrr

--- src/main/java/com/donatoordep/anime_list_api/security/JWTAuthenticationFilter.java
+++ src/main/java/com/donatoordep/anime_list_api/security/JWTAuthenticationFilter.java
@@ -37,7 +37,26 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
             SecurityContextHolder.getContext().setAuthentication(
                     new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities()));
         }
+        corsConfigurer(response);
         filterChain.doFilter(request, response);
+    }
+
+    private void corsConfigurer(HttpServletResponse response) {
+        if (response.getHeader("Access-Control-Allow-Origin") == null) {
+            response.addHeader("Access-Control-Allow-Origin", "*");
+        }
+
+        if (response.getHeader("Access-Control-Allow-Headers") == null) {
+            response.addHeader("Access-Control-Allow-Headers", "*");
+        }
+
+        if (response.getHeader("Access-Control-Request-Headers") == null) {
+            response.addHeader("Access-Control-Request-Headers", "*");
+        }
+
+        if (response.getHeader("Access-Control-Allow-Methods") == null) {
+            response.addHeader("Access-Control-Allow-Methods", "*");
+        }
     }
 
     // Recuperar o token JWT do cabeçalho da requisição
