869ceeeffc484c74fb7c06fdd70fc96d13af1fb4
Pedro Donato <emagdonatdonatoordep@gmail.com>
2023-08-18 10:31:58 -0300

    unit test for authcontroller (Err)

--- src/test/java/com/donatoordep/anime_list_api/controllers/AuthControllerTest.java
+++ src/test/java/com/donatoordep/anime_list_api/controllers/AuthControllerTest.java
@@ -7,43 +7,50 @@ import com.donatoordep.anime_list_api.dto.response.CartResponseDTO;
 import com.donatoordep.anime_list_api.dto.response.ProfileUserResponseDTO;
 import com.donatoordep.anime_list_api.dto.response.UserResponseDTO;
 import com.donatoordep.anime_list_api.entities.User;
+import com.donatoordep.anime_list_api.mappers.UserMapper;
+import com.donatoordep.anime_list_api.repositories.UserRepository;
+import com.donatoordep.anime_list_api.services.AnimeService;
+import com.donatoordep.anime_list_api.services.RoleService;
 import com.donatoordep.anime_list_api.services.UserService;
+import com.donatoordep.anime_list_api.services.business.rules.user.register.RegisterUserValidation;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.ResultMatcher;
 
-import static net.bytebuddy.matcher.ElementMatchers.is;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(UserController.class)
+@WebMvcTest
 public class AuthControllerTest {
 
     @Autowired
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
 
     @Autowired
-    ObjectMapper mapper;
+    private ObjectMapper mapper;
 
     @MockBean
-    UserService service;
+    private UserService service;
 
-    User user;
-    UserResponseDTO userResponseDTO;
-    UserRequestDTO userRequestDTO;
+    private User user;
+    private UserResponseDTO userResponseDTO;
+    private UserRequestDTO userRequestDTO;
 
     @BeforeEach
     void setup() {
@@ -69,16 +76,17 @@ public class AuthControllerTest {
     @Test
     @DisplayName("Display name")
     void testABCD_When_XYZ_ShouldReturn_FSAD() throws Exception {
+        UserResponseDTO expectedResponse = userResponseDTO; // O UserResponseDTO esperado
 
         when(service.register(any(UserRequestDTO.class)))
-                .thenAnswer((invocation) -> invocation.getArgument(0));
+                .thenReturn(expectedResponse); // Configuração do mock para retornar o UserResponseDTO
 
         ResultActions response = mockMvc.perform(post("/v1/auth/register")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(user)));
+                .content(mapper.writeValueAsString(userRequestDTO))); // Use userRequestDTO aqui
 
         response.andDo(print())
                 .andExpect(status().isOk())
-                .andExpect((ResultMatcher) jsonPath("$.email", is(user.getEmail())));
+                .andExpect(jsonPath("$.email", is(expectedResponse.getEmail())));
     }
 }
